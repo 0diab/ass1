fca8e3bc9d54c463a0b64c463bb9d712c016b069
amoora <omardiab.m@gmail.com>
2025-01-21 14:48:06 -0500

    spiderman

--- Assignment1A.py
+++ Assignment1A.py
@@ -1,3 +1,4 @@
+print(f"[HTML Alert Generator]")
 alert = input(f"Enter the alert message:\n")
 print(f"\nThis is the custom HTML Alert:\n<div class=”g-alert”><p>{alert}")
 #Alert: Due to routine maintenance, some of our services may be unavailable on January 19th between 7 AM and 12 PM ET.
