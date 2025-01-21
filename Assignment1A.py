0a3dfd89ec7bc61f10056ce2fb5e289cdaed52f2
amoora <omardiab.m@gmail.com>
2025-01-21 14:25:02 -0500

    spiderman

--- Assignment1A.py
+++ Assignment1A.py
@@ -1,2 +1,3 @@
 alert = input(f"Enter the alert message:\n")
 print(f"\nThis is the custom HTML Alert:\n<div class=”g-alert”><p>{alert}")
+#Alert: Due to routine maintenance, some of our services may be unavailable on January 19th between 7 AM and 12 PM ET.
--- Assignment1C.py
+++ Assignment1C.py
@@ -1,6 +1,6 @@
 print(f"[Centimeters to Feet and Inches Converter]")
 cm = float(input(f"Enter the length in centimeters: "))
 ft = cm / 30.48
-inch = round((ft % 1)*12,2)
+inch = (ft % 1)*12
 ft = int(ft)
-print(f"\nThe length is {ft} feet and {inch} inches")
+print(f"\nThe length is {ft} feet and {inch:.2f} inches")
